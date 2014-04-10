8a64a0909264f2c759d073416bce398f3184e6e3
Marty Fajardo & Tara Scherner de la Fuente <student@epicodus.com>
2014-04-10 16:50:34 -0700

    fixes photo new path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Photr::Application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'post', to: , 'pictures_new#new' as: 'post'
-
+  get 'post', to: 'pictures_new#new', as: 'post'
 
   resources :users
   resources :sessions
