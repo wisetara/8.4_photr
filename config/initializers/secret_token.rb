f323ca63382c79431244214174312d7e43b13e1d
Marty Fajardo & Tara Scherner de la Fuente <student@epicodus.com>
2014-04-10 10:56:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photr::Application.config.secret_key_base = 'd718344b5d61d29fe6e8575456c893acb3de9f0389f576ae737b25a3751638cf8ee9eb644e3bad7e865b9d0f385734e7ae1b1df9214414e70ccd71e1097c7016'
